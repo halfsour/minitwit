1964886c9182e2007b51c91599a331ee15eebfc0
Half Sour <mpl@crito.net>
2016-09-20 15:39:00 -0400

    move all curl calls to function to facilitate common retry

--- ci/test-integration.sh
+++ ci/test-integration.sh
@@ -15,6 +15,22 @@ COOKIE_JAR="cookies-$(date | md5sum | head -c 10).txt"
 
 CONTAINER_ID="$(docker run -d "${DOCKER_IMG}")"
 
+curl_try (){
+	CMD=${1}
+	i="0"
+	while [[ ${i} < 200 ]]; do
+		set +o errexit
+		STATUS_CODE=${CMD}
+		set -o errexit
+		if [[ "200" == "${STATUS_CODE}" ]]; then
+		       	break
+	       	fi 
+		i=$[$i+1]
+		sleep 2
+	done
+}
+
+
 function cleanup {
   rm -f "${COOKIE_JAR}"
   docker rm -f "$CONTAINER_ID"
@@ -23,44 +39,23 @@ trap cleanup EXIT
 
 CONTAINER_IP="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CONTAINER_ID})"
 
-i="0"
-while [[ ${i} < 200 ]]; do
-  set +o errexit
-  STATUS_CODE="$(curl -I -s "http://${CONTAINER_IP}/public" | grep "HTTP/1.1" | cut -d' ' -f2)"
-  set -o errexit
-  if [[ "200" == "${STATUS_CODE}" ]]; then
-    break
-  fi
-  i=$[$i+1]
-  sleep 2
-done
+public="$(curl -I -s "http://${CONTAINER_IP}/public" | grep "HTTP/1.1" | cut -d' ' -f2)"
+curl_try ${public}
 
 USERNAME="$(date | md5sum | head -c 10)"
 
-curl -v -f -X POST \
-  --data "username=${USERNAME}&email=test@mailinator.com&password=password&password2=password" \
-  "http://${CONTAINER_IP}/register"
+register_tst="$(curl -v -f -X POST --data "username=${USERNAME}&email=test@mailinator.com&password=password&password2=password" "http://${CONTAINER_IP}/register)"
+curl_try ${register_tst}
 
-curl -v -f -X POST -c "${COOKIE_JAR}" \
-  --data "username=${USERNAME}&password=password" \
-  "http://${CONTAINER_IP}/login"
+login_tst="$(curl -v -f -X POST -c "${COOKIE_JAR}" --data "username=${USERNAME}&password=password" "http://${CONTAINER_IP}/login")"
+curl_try ${login_tst}
 
 MESSAGE="$(date | md5sum | head -c 10)"
 
-curl -v -f -X POST -b "${COOKIE_JAR}" \
-  --data "text=secret-test-message" \
-  "http://${CONTAINER_IP}/message"
+msg_tst="$(curl -v -f -X POST -b "${COOKIE_JAR}" -data "text=secret-test-message" "http://${CONTAINER_IP}/message")"
+curl_try ${msg_tst}
 
-i="0"
-set +o errexit
-while [[ ${i} < 100 ]]; do
-	STATUS_CODE="$(curl -v -f "http://${CONTAINER_IP}/public" | grep -s "${MESSAGE}")"
-	if [[ "200" == "${STATUS_CODE}" ]];then
-		set -o errexit
-		break
-	fi
-	i=[$i+1]
-	sleep 1
-done
+getmsg_tst="$(curl -v -f "http://${CONTAINER_IP}/public" | grep -s "${MESSAGE}")"
+curl_try ${getmsg_tst}
 
 echo "TEST PASSED"

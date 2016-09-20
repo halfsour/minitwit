aaf86cc65533f5f5b7bd262f405c6ea6dd1755e9
Half Sour <mpl@crito.net>
2016-09-20 16:42:11 -0400

    more debug

--- ci/test-integration.sh
+++ ci/test-integration.sh
@@ -41,8 +41,7 @@ trap cleanup EXIT
 
 CONTAINER_IP="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CONTAINER_ID})"
 
-public="curl -I -s \"http://${CONTAINER_IP}/public\" | grep \"HTTP/1.1\" | cut -d' ' -f2)"
-
+public="curl -I -s "http://${CONTAINER_IP}/public" | grep "HTTP/1.1" | cut -d' ' -f2)"
 curl_try ${public}
 
 USERNAME="$(date | md5sum | head -c 10)"
